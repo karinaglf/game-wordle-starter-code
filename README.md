947b11990edda15c701488c28d15b5933ee696bc
Karina Freitas <karinaglf@gmail.com>
2022-03-23 13:34:46 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# My Wordle Game
+<br>
+
+## Description
+Starter Code for Build Your Game with Javascript Online Workshop held by Ironhack Lisbon
+
+## Details
+Creating a game is a fun way to level up your programming learning and expand your development skills. 
+
+In this quick course, you'll learn how to ***build a Wordle clone using only HTML5, Javascript, and CSS.*** Wordle is a viral word game that has become a daily routine in many people’s lives. Based on a step-by-step coding tutorial, the instructor will cover many key programming concepts like DOM Manipulation, arrays and its methods, conditional statements, creating custom functions, and much more.
+
+## Workshop Recording
+- [Workshop Link](https://event.eu.on24.com/wcc/r/8000014562/4BA31781B90B320C83C41880C9DA321F?fbclid=IwAR29g4-xd0A_ZS1tSLeD5qB5agK2puZd4GW204-BHoVWVmyOwJhaddudqIg)
+
---
+++ solution/app-solution.js
@@ -0,0 +1,148 @@
+// DOM Elements => Game Area
+const board = document.getElementById('game-board');
+const message = document.getElementById('message');
+const keyboard = document.getElementById('keyboard');
+
+// Game Variables
+const keys = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '⌫'];
+
+const wordList = words;
+const targetWord = wordList[Math.floor(Math.random() * wordList.length)];
+
+const gameBoard = [
+	['', '', '', '', ''],
+	['', '', '', '', ''],
+	['', '', '', '', ''],
+	['', '', '', '', ''],
+	['', '', '', '', ''],
+	['', '', '', '', ''],
+];
+let currentRow = 0;
+let currentPosition = 0;
+let guessedWord = '';
+let isGameOver = false;
+
+// Functions
+function drawBoard() {
+	gameBoard.forEach((oneRow, index) => {
+		const row = document.createElement('div');
+		row.setAttribute('id', `row-${index}`);
+		row.className = 'row';
+		board.appendChild(row);
+
+		oneRow.forEach((oneCard, i) => {
+			const card = document.createElement('div');
+			card.setAttribute('id', `row-${index}-card-${i}`);
+			card.classList.add('card');
+			card.innerText = oneCard;
+			row.appendChild(card);
+		});
+	});
+}
+
+function drawKeyboard() {
+	keys.forEach((key) => {
+		const btnKey = document.createElement('button');
+		btnKey.innerHTML = key;
+		btnKey.setAttribute('value', key);
+		btnKey.setAttribute('id', `key-${key}`);
+		btnKey.addEventListener('click', () => handleClick(key));
+		keyboard.appendChild(btnKey);
+	});
+}
+
+function startGame() {
+	drawKeyboard();
+	drawBoard();
+}
+
+function startNewGuess() {
+	message.innerText = '';
+
+	currentPosition = 0;
+	currentRow++;
+
+	if (currentRow > 5 && !isGameOver) {
+		message.innerText = `You missed, the word was ${targetWord.toUpperCase()}`;
+		isGameOver = true;
+	}
+}
+
+function handleClick(letter) {
+	if (letter === '⌫') {
+		deleteLetter();
+	} else if (letter === 'ENTER') {
+		submitGuess();
+	} else {
+		addLetter(letter);
+	}
+}
+
+function addLetter(letter) {
+	if (currentPosition < 5 && currentRow <= 6) {
+		//Update DOM
+		const cardElement = document.getElementById(`row-${currentRow}-card-${currentPosition}`);
+		cardElement.innerText = letter;
+		cardElement.classList.add('has-letter');
+
+		//Update game controllers
+		gameBoard[currentRow][currentPosition] = letter;
+		return currentPosition++;
+	}
+}
+
+function deleteLetter() {
+	message.innerText = '';
+
+	if (currentPosition > 0 && currentPosition <= 5) {
+		//Update DOM
+		const cardElement = document.getElementById(`row-${currentRow}-card-${currentPosition - 1}`);
+		cardElement.innerText = '';
+		cardElement.classList.remove('has-letter');
+
+		// Update game controllers
+		gameBoard[currentRow][currentPosition] = '';
+		return currentPosition--;
+	}
+}
+
+function submitGuess() {
+	if (currentPosition < 5) return (message.innerText = 'Not long enough');
+
+	guessedWord = gameBoard[currentRow].join('').toLowerCase();
+	console.log(guessedWord);
+
+	if (guessedWord === targetWord) {
+		isGameOver = true;
+		checkLetters();
+		message.innerText = 'You got it!';
+	} else if (!wordList.includes(guessedWord)) {
+		message.innerText = 'Not in word list!';
+	} else {
+		checkLetters();
+	}
+}
+
+function checkLetters() {
+	const guessedLetters = gameBoard[currentRow];
+
+	guessedLetters.forEach((letter, i) => {
+		const card = document.getElementById(`row-${currentRow}-card-${i}`);
+		const btnKey = document.getElementById(`key-${letter}`);
+
+		if (targetWord[i] === letter.toLowerCase()) {
+			card.classList.add('correct');
+			btnKey.classList.add('correct');
+		} else if (targetWord.includes(letter.toLowerCase())) {
+			card.classList.add('wrong-place');
+			btnKey.classList.add('wrong-place');
+		} else {
+			card.classList.add('wrong');
+			btnKey.classList.add('wrong');
+		}
+	});
+	startNewGuess();
+}
+
+//
+startGame();
